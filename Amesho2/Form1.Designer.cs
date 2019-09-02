e02fc44d4027ae8a67838922b4e56a8847176206
E.MATSUURA <icsneshimoi@gmail.com>
2019-09-02 13:59:00 +0900

    初回登録 (2016年年末ごろ作ったツール。)

---
+++ Amesho2/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Amesho2
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtExcelFile = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colSheetName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRHeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLFooter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCFooter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRFooter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(854, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "選択";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "編集対象のExcelファイル";
+            // 
+            // txtExcelFile
+            // 
+            this.txtExcelFile.Location = new System.Drawing.Point(12, 59);
+            this.txtExcelFile.Name = "txtExcelFile";
+            this.txtExcelFile.Size = new System.Drawing.Size(836, 19);
+            this.txtExcelFile.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colSheetName,
+            this.colLHeader,
+            this.colCHeader,
+            this.colRHeader,
+            this.colLFooter,
+            this.colCFooter,
+            this.colRFooter});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(898, 321);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // colSheetName
+            // 
+            this.colSheetName.HeaderText = "シート名";
+            this.colSheetName.Name = "colSheetName";
+            this.colSheetName.ReadOnly = true;
+            this.colSheetName.Width = 150;
+            // 
+            // colLHeader
+            // 
+            this.colLHeader.HeaderText = "左上";
+            this.colLHeader.Name = "colLHeader";
+            // 
+            // colCHeader
+            // 
+            this.colCHeader.HeaderText = "中央上";
+            this.colCHeader.Name = "colCHeader";
+            // 
+            // colRHeader
+            // 
+            this.colRHeader.HeaderText = "右上";
+            this.colRHeader.Name = "colRHeader";
+            // 
+            // colLFooter
+            // 
+            this.colLFooter.HeaderText = "左下";
+            this.colLFooter.Name = "colLFooter";
+            // 
+            // colCFooter
+            // 
+            this.colCFooter.HeaderText = "中央下";
+            this.colCFooter.Name = "colCFooter";
+            // 
+            // colRFooter
+            // 
+            this.colRFooter.HeaderText = "右下";
+            this.colRFooter.Name = "colRFooter";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(12, 436);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "設定変更開始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "進行状況(対応済/全シート数)";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(325, 441);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(454, 441);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 6;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 504);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtExcelFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "FormMain";
+            this.Text = "Excelファイルのヘッダフッタを一括設定するツール";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtExcelFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSheetName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLHeader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCHeader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRHeader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLFooter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCFooter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRFooter;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Amesho2/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace Amesho2
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+
+            // DataGridView1にユーザーが新しい行を追加できないようにする
+            dataGridView1.AllowUserToAddRows = false;
+        }
+
+        /// <summary>
+        /// [参照]ボタンクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "Excelファイル|*.xls;*.xlsx;*.xlsm";
+            ofd.Title = "編集対象のExcelファイルを選択してください。";
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtExcelFile.Text = ofd.FileName;
+            }
+
+            // Excelファイル展開
+            ShowHeaderFooter(txtExcelFile.Text);
+        }
+
+        /// <summary>
+        /// Excelファイル展開し、グリッドに表示
+        /// </summary>
+        /// <param name="strPath">Excelファイル名(フルパス)</param>
+        private void ShowHeaderFooter(string strPath)
+        {
+            if (strPath == "") { return; }
+
+            // 待機状態
+            Cursor.Current = Cursors.WaitCursor;
+
+            dataGridView1.Rows.Clear();
+            
+            Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+            // エクセルを非表示
+            ExcelApp.Visible = false;
+
+            // エクセルファイルのオープン
+            Microsoft.Office.Interop.Excel.Workbook WorkBook = ExcelApp.Workbooks.Open(strPath);
+
+            int iRowCnt = 0;
+            for (int i = 0; i < WorkBook.Sheets.Count; i++)
+            {
+                // シートの選択
+                Microsoft.Office.Interop.Excel.Worksheet sheet = (Microsoft.Office.Interop.Excel.Worksheet)WorkBook.Sheets[(i + 1)];
+
+                if ((sheet.Visible == Microsoft.Office.Interop.Excel.XlSheetVisibility.xlSheetHidden)
+                 || (sheet.Visible == Microsoft.Office.Interop.Excel.XlSheetVisibility.xlSheetVeryHidden))
+                {
+                    continue;
+                }
+
+                sheet.Select();
+
+                dataGridView1.Rows.Add();
+
+                dataGridView1.Rows[iRowCnt].Cells["colSheetName"].Value = sheet.Name;
+                dataGridView1.Rows[iRowCnt].Cells["colLHeader"].Value = sheet.PageSetup.LeftHeader;
+                dataGridView1.Rows[iRowCnt].Cells["colCHeader"].Value = sheet.PageSetup.CenterHeader;
+                dataGridView1.Rows[iRowCnt].Cells["colRHeader"].Value = sheet.PageSetup.RightHeader;
+                dataGridView1.Rows[iRowCnt].Cells["colLFooter"].Value = sheet.PageSetup.LeftFooter;
+                dataGridView1.Rows[iRowCnt].Cells["colCFooter"].Value = sheet.PageSetup.CenterFooter;
+                dataGridView1.Rows[iRowCnt].Cells["colRFooter"].Value = sheet.PageSetup.RightFooter;
+                iRowCnt++;
+            }
+
+            // workbookを閉じる
+            try 
+            {
+                WorkBook.Close(SaveChanges: false);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(WorkBook);
+            }
+
+            // エクセルを閉じる
+            try 
+            {
+                ExcelApp.Quit();
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(ExcelApp);
+            }
+            // 元に戻す
+            Cursor.Current = Cursors.Default;
+        }
+
+        /// <summary>
+        /// [設定変更開始]ボタンクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 待機状態
+            Cursor.Current = Cursors.WaitCursor;
+
+            Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+            // エクセルを非表示
+            ExcelApp.Visible = false;
+
+            // エクセルファイルのオープン
+            Microsoft.Office.Interop.Excel.Workbook WorkBook = ExcelApp.Workbooks.Open(txtExcelFile.Text);
+
+            int i = 0;
+
+            label4.Text = i.ToString() + " / " + dataGridView1.Rows.Count.ToString();
+
+            for ( ; i < dataGridView1.Rows.Count; i++)
+            {
+                string strSheetName = dataGridView1.Rows[i].Cells["colSheetName"].Value.ToString();
+
+                // シートの選択
+                Microsoft.Office.Interop.Excel.Worksheet sheet = (Microsoft.Office.Interop.Excel.Worksheet)WorkBook.Worksheets[strSheetName];
+                sheet.Select();
+
+                sheet.PageSetup.LeftHeader = Convert.ToString(dataGridView1.Rows[i].Cells["colLHeader"].Value);
+                sheet.PageSetup.CenterHeader = Convert.ToString(dataGridView1.Rows[i].Cells["colCHeader"].Value);
+                sheet.PageSetup.RightHeader = Convert.ToString(dataGridView1.Rows[i].Cells["colRHeader"].Value);
+                sheet.PageSetup.LeftFooter = Convert.ToString(dataGridView1.Rows[i].Cells["colLFooter"].Value);
+                sheet.PageSetup.CenterFooter = Convert.ToString(dataGridView1.Rows[i].Cells["colCFooter"].Value);
+                sheet.PageSetup.RightFooter = Convert.ToString(dataGridView1.Rows[i].Cells["colRFooter"].Value);
+
+                label4.Text = (i+1).ToString() + " / " + dataGridView1.Rows.Count.ToString();
+                label4.Update();
+            }
+
+            // 保存メッセージを出さないようにする
+            ExcelApp.DisplayAlerts = false;
+            
+            // 保存
+            WorkBook.Save();
+
+            // workbookを閉じる
+            try 
+            {
+                WorkBook.Close();
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(WorkBook);
+            }
+
+            // エクセルを閉じる
+            try 
+            {
+                ExcelApp.Quit();
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(ExcelApp);
+            }
+
+            // 元に戻す
+            Cursor.Current = Cursors.Default;
+
+
+            MessageBox.Show("ヘッダー・フッターの編集が完了しました。");
+        }
+    }
+}
